298c7ac2ecf87eac21b23da590280e0b7213000c
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-01-30 15:22:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,20 +21,6 @@ const Home = () => {
       },
       body: JSON.stringify({ userInput }),
     });
-
-    const data = await response.json();
-    const { output } = data;
-    console.log("OpenAI replied...", output.text)
-
-    setApiOutput(`${output.text}`);
-    setIsGenerating(false);
-  }
-  const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 250,
-  });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
 
