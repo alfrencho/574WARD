0f98c954c4a23ff203c6b58537b8fd691c2618ee
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-04 22:18:00 +0000

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { OpenAIApi } from 'openai';
+import React, { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
-  
   const [apiOutput, setApiOutput] = useState('');
-const [isGenerating, setIsGenerating] = useState(false)
+  const [isGenerating, setIsGenerating] = useState(false);
 
-const callGenerateEndpoint = async () => {
-  setIsGenerating(true);
- 
-  
-  console.log("Calling OpenAI...")
-  const response = await fetch('/api/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({ userInput }),
-  });
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
 
-  const data = await response.json();
-  const { output } = data;
-  console.log("OpenAI replied...", output.text)
+    console.log("Calling OpenAI...");
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({ userInput }),
+    });
 
-  setApiOutput(`${output.text}`);
-  setIsGenerating(false);
-}
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <div className="container">
@@ -41,11 +43,12 @@ const callGenerateEndpoint = async () => {
         </div>
         <div className="prompt-container">
         </div>
-       <textarea
-  placeholder="start typing here"
-  className="prompt-box"
-  value={userInput}
-  onChange={onUserChangedText} />
+        <textarea
+          placeholder="start typing here"
+          className="prompt-box"
+          value={userInput}
+          onChange={onUserChangedText}
+        />
         <div className="prompt-buttons">
           <a
             className={isGenerating ? 'generate-button loading' : 'generate-button'}
@@ -53,19 +56,18 @@ const callGenerateEndpoint = async () => {
           >
             <div className="generate">
               {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
-  {apiOutput && (
-  <div className="output">
-    <div className="output-header-container">
-      <div className="output-header">
-        <h3>Output</h3>
-      </div>
-    </div>
-    <div className="output-content">
-      <p>{apiOutput}</p>
-    </div>
-  </div>
-)}
-</div>
+              {apiOutput && (
+                <div className="output">
+                  <div className="output-header-container">
+                    <div className="output-header">
+                      <h3>Output</h3>
+                    </div>
+                  </div>
+                  <div className="output-content">
+                    <p>{apiOutput}</p>
+                  </div>
+                </div>
+              )}
             </div>
           </a>
         </div>
@@ -73,5 +75,3 @@ const callGenerateEndpoint = async () => {
     </div>
   );
 };
- 
-export default Home;
