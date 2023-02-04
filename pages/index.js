cafb4577aea6efc1bb6e0f781bb9b1b6121e9954
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-04 12:39:42 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { OpenAIApi } from 'openai';
-
-const Home = () => {
-  const [userInput, setUserInput] = useState('');
-  const [apiOutput, setApiOutput] = useState('');
-  const [isGenerating, setIsGenerating] = useState(false);
-
-  const onUserChangedText = (event) => {
-    setUserInput(event.target.value);
-  }
-
-  const callGenerateEndpoint = async () => {
-    setIsGenerating(true);
-    
-    console.log("Calling OpenAI...")
-    const response = await fetch('/api/generate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-      },
-      body: JSON.stringify({ userInput }),
-    });
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
   
-    const data = await response.json();
-    const { output } = data;
-    console.log("OpenAI replied...", output.text)
-  
-    setApiOutput(`${output.text}`);
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  // Check if response begins with stop sequence
+  const stopSequence = "<STOP SEQUENCE>";
+  if (output.text.startsWith(stopSequence)) {
+    console.log("Response begins with stop sequence, ignoring...")
     setIsGenerating(false);
+    return;
   }
-
-  return (
-    <div className="root">
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>574WARD Marketing Assistant</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Build The Bend</h2>
-          </div>
-        </div>
-        <div className="prompt-container">
-        </div>
-        <textarea
-          placeholder="start typing here"
-          className="prompt-box"
-          value={userInput}
-          onChange={onUserChangedText} />
-        <div className="prompt-buttons">
-          <a
-            className={isGenerating ? 'generate-button loading' : 'generate-button'}
-            onClick={callGenerateEndpoint}
-          >
-            <div className="generate">
-              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
-            </div>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
+  
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
 
 
  
