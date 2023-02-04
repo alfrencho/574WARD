e01f1a3140ce74b51ef7c7254c2bef51e497508c
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-04 13:47:07 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,6 +55,19 @@ const callGenerateEndpoint = async () => {
           >
             <div className="generate">
               {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+</div>
             </div>
           </a>
         </div>
