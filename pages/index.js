893a1993b7d4405bf0913045667203a9a34d2e9c
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-03 21:55:02 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ const Home = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       },
       body: JSON.stringify({ userInput }),
     });
