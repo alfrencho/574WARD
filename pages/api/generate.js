404b8f4e5b4429c7042952d5d95902a746916cab
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-05 23:24:24 -0500

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -2,7 +2,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-apiKey: process.env.OPENAI_API_KEY,
+apiKey = OPENAI_API_KEY: ${{ secrets.API_KEY },
 });
 
 // Creates an OpenAIApi instance using the Configuration instance
