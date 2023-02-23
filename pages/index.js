91be22f0a61cfcc5f50833590fd07d5a298ef605
David Fisher <123786348+alfrencho@users.noreply.github.com>
2023-02-22 21:55:19 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,29 @@
-import { useState } from 'react';
-
-const Home = () => {
-  const [userInput, setUserInput] = useState("");
-  const onUserChangedText = event => {
-    setUserInput(event.target.value);
-  };
-  
-  const [apiOutput, setApiOutput] = useState('');
-  const [isGenerating, setIsGenerating] = useState(false);
-  
-  useEffect(() => {
-    const callGenerateEndpoint = async () => {
-      setIsGenerating(true);
-    
-      console.log("Calling OpenAI...")
-      const response = await fetch('/api/generate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ userInput }),
-      });
-    
-      const data = await response.json();
-      const { output } = data;
-      console.log("OpenAI replied...", output.text)
-    
-      setApiOutput(`${output.text}`);
-      setIsGenerating(false);
-    };
-    
-    callGenerateEndpoint();
-  }, [userInput]);
-  
-  return (
-    <div className="root">
-      <Head>
-        <title>574WARD Marketing Assistant</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>574WARD Marketing Assistant</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Build The Bend</h2>
-          </div>
-       
-
-        <div className="prompt-container">
-        <textarea
-  className="prompt-box"
-  placeholder="start typing here"
-  value={userInput}
-  onChange={onUserChangedText}
-/>;
-<div className="prompt-buttons">
-    <a className="generate-button" onClick={callGenerateEndpoint}>
-      <div className="generate">
-        <p>Generate</p>
-      </div>
-    </a>
-  </div>
-    {apiOutput && (
-  <div className="output">
-    <div className="output-header-container">
-      <div className="output-header">
-        <h3>Output</h3>
-      </div>
-    </div>
-    <div className="output-content">
-      <p>{apiOutput}</p>
-    </div>
-  </div>
-)}
-</div>
+return (
+  <div className="root">
+    <Head>
+      <title>574WARD Marketing Assistant</title>
+    </Head>
+    <div className="container">
+      <div className="header">
+        <div className="header-title">
+          <h1>574WARD Marketing Assistant</h1>
+        </div>
+        <div className="header-subtitle">
+          <h2>Build The Bend</h2>
         </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
+      <iframe src="https://beta.pickaxeproject.com/axe?id=RRB454HSCG2AE5NEC7XH&mode=embed&theme=dark&opacity=100" width="100%" height="600px" onMouseOver="this.style.boxShadow='0 2px 2px 1px rgba(0,0,0,0.2)'" onMouseOut="this.style.boxShadow='none'" style={{ border: 'none', transition: '.3s' }}></iframe>
     </div>
-   
-  );
-};
+    <div className="badge-container grow">
+      <a href="https://buildspace.so/builds/ai-writer" target="_blank" rel="noreferrer">
+        <div className="badge">
+          <p>build with buildspace</p>
+        </div>
+      </a>
+    </div>
+  </div>
+);
 
-export default Home;
 
 
 
